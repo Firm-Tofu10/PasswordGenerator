cc0fbdc930a9c4393b52014240162881833e58eb
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-07-27 17:07:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-const lowerCase = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+// const lowerCase = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
 
-const coolSymbol = ['!','@','#','$','%','^','&','*'];
+// const coolSymbol = ['!','@','#','$','%','^','&','*'];
 
-const uperCase =  ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
+// const uperCase =  ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
 
-const coolNumber = ['1','2','3','4','5','6','7','8','9','0']
+// const coolNumber = ['1','2','3','4','5','6','7','8','9','0']
 
-function Generation = lowerCase[Math.floor(Math.random() * characterAmount.length)];
+// function Generation = lowerCase[Math.floor(Math.random() * characterAmount.length)];
 
-function checkboxUpercase = uperCase[Math.floor(Math.random() * characterAmount.length)];
+// function checkboxUpercase = uperCase[Math.floor(Math.random() * characterAmount.length)];
 
-function checkboxSymbol = coolSymbol[Math.floor(Math.random() * characterAmount.length)];
+// function checkboxSymbol = coolSymbol[Math.floor(Math.random() * characterAmount.length)];
 
-function checkboxNumber = coolNumber[Math.floor(Math.random() * characterAmount.length)];
+// function checkboxNumber = coolNumber[Math.floor(Math.random() * characterAmount.length)];
 
 //I want to make a Funcation that takes into account what is and is not checked, then math randoms the character amounts to the 4 options (C) then have the arrays funtions receive those valuse then push (C) to generated charcters to each function with the correct (C).
 // (C) = the charicter amounts sent to each of the 4 options randmoly probly using math random.
+element.addEventListener("click",function(pressPush){
+    document.getElementById("demo").innerHTML = "Im here";
+});
 
+function pressPush(){
+    document.getElementById("demo").innerHTML = "Im here";
+}
