5ced53382870cc6b7b49f89faf72be3d23726320
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-08-25 00:03:15 -0700

    asdasdas
    asdasdas

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// const lowerCase = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+const lowerCase = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
 
-// const coolSymbol = ['!','@','#','$','%','^','&','*'];
+const coolSymbol = ['!','@','#','$','%','^','&','*'];
 
-// const uperCase =  ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
+const uperCase =  ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
 
-// const coolNumber = ['1','2','3','4','5','6','7','8','9','0']
+const coolNumber = ['1','2','3','4','5','6','7','8','9','0']
 
 // function Generation = lowerCase[Math.floor(Math.random() * characterAmount.length)];
 
@@ -17,6 +17,8 @@
 //I want to make a Funcation that takes into account what is and is not checked, then math randoms the character amounts to the 4 options (C) then have the arrays funtions receive those valuse then push (C) to generated charcters to each function with the correct (C).
 // (C) = the charicter amounts sent to each of the 4 options randmoly probly using math random.
 
-function pressPush(){document.getElementById("btnGenerate").addEventListener("click",displayDate)};
-console.log(btnGenerate);
+// function gen(){document.getElementById("btnGenerate").addEventListener("onclick",)
+// console.log("generate")
+// };
 
+function gen() {console.log("gen")};
