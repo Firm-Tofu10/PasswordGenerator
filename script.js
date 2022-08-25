b98adcb7aa2ffffb839fd72139ffe88ec5e77327
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-08-25 16:42:23 -0700

    Trying to get my check boxes to work
    Trying to implement this  via one function but seems to not be fuctioning at the moment.

--- script.js
+++ script.js
@@ -1,24 +1,64 @@
-const lowerCase = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+//The following arrays are used to specify the character being inserted to the generator output.
+const lowerCase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 
-const coolSymbol = ['!','@','#','$','%','^','&','*'];
+const coolSymbol = ['!', '@', '#', '$', '%', '^', '&', '*'];
 
-const uperCase =  ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
+const uperCase = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
 
-const coolNumber = ['1','2','3','4','5','6','7','8','9','0']
+const coolNumber = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
-// function Generation = lowerCase[Math.floor(Math.random() * characterAmount.length)];
+// var characterAmount = document.getElementById("number-input");
 
-// function checkboxUpercase = uperCase[Math.floor(Math.random() * characterAmount.length)];
-
-// function checkboxSymbol = coolSymbol[Math.floor(Math.random() * characterAmount.length)];
-
-// function checkboxNumber = coolNumber[Math.floor(Math.random() * characterAmount.length)];
-
-//I want to make a Funcation that takes into account what is and is not checked, then math randoms the character amounts to the 4 options (C) then have the arrays funtions receive those valuse then push (C) to generated charcters to each function with the correct (C).
-// (C) = the charicter amounts sent to each of the 4 options randmoly probly using math random.
-
-// function gen(){document.getElementById("btnGenerate").addEventListener("onclick",)
-// console.log("generate")
+// function gen() {
+// 	var password = "";
+// 	for (i = 0; i <= characterAmount.ariaValueMax; i++) {
+// 		if (checkbox.checked) {
+// 		}
+// 	}
 // };
 
-function gen() {console.log("gen")};
+function checkCheckbox(element) {
+	const checkbox = document.getElementById(element);
+	if (checkbox.checked) {
+		console.log("true");
+		return true;
+	}
+	else {
+		console.log("false");
+		return false;
+	}
+};
+
+
+	// const removeRandom = (lowerCase) => {
+	// 	while (array.length) {
+	// 		const random = Math.floor(Math.random() * array.length);
+	// 		const el = array.splice(random, 1)[0];
+	// 		console.log(el);
+	// 	}
+	// };
+
+	//The for loops below are used to generate the random arrays to the required length.
+
+	// for (var i = 0; i <= passwordLength; i++) {
+	// 	var randomNumber = Math.floor(Math.random() * lowerCase.length);
+	// 	password += chars.substring(randomNumber, randomNumber + 1);
+	// }
+
+	// for (var i = 0; i <= passwordLength; i++) {
+	// 	var randomNumber = Math.floor(Math.random() * upperCase.length);
+	// 	password += chars.substring(randomNumber, randomNumber + 1);
+	// }
+
+	// for (var i = 0; i <= passwordLength; i++) {
+	// 	var randomNumber = Math.floor(Math.random() * coolSymbol.length);
+	// 	password += chars.substring(randomNumber, randomNumber + 1);
+	// }
+
+	// for (var i = 0; i <= passwordLength; i++) {
+	// 	var randomNumber = Math.floor(Math.random() * coolNumber.length);
+	// 	password += chars.substring(randomNumber, randomNumber + 1);
+	// }
+
+// function gen(){document.getElementById("btnGenerate").addEventListener("onclick")};
+
